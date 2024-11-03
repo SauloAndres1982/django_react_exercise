4c1ebbe8c3abacc6f5b09ab0305b10654b19511f
SauloAndres1982 <saulo.carmona@soyrosarista.edu.co>
2024-11-03 13:25:48 -0500

    primer commit

---
+++ core/routers.py
@@ -0,0 +1,8 @@
+from rest_framework import routers
+from .views import UserViewSet
+
+router = routers.SimpleRouter()
+
+router.register(r'user', UserViewSet, basename='user')
+
+urlpatterns = [*router.urls,]
